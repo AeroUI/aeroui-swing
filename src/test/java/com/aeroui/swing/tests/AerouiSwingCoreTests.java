21391b5e33198cfe02ebd764e70eec1c596c0740
shabman <avia.shabbyman@gmail.com>
2023-08-28 21:13:53 +0400

    setup tests

---
+++ src/test/java/com/aeroui/swing/tests/AerouiSwingCoreTests.java
@@ -0,0 +1,10 @@
+package com.aeroui.swing.tests;
+
+public class AerouiSwingCoreTests {
+
+    protected  AerouiSwingCoreTests() { }
+
+    public static void main(String[] args) {
+        System.out.println("AERO UI: TESTS");
+    }
+}
