645271f777645ea6c5ba78405e4327466270cfe6
shabman <avia.shabbyman@gmail.com>
2023-08-28 20:59:03 +0400

    first push

---
+++ src/main/java/com/aeroui/swing/laf/core/AerouiSwing.java
@@ -0,0 +1,12 @@
+package com.aeroui.swing.laf.core;
+
+import javax.swing.LookAndFeel;
+
+public class AerouiSwing {
+
+    private AerouiSwing() { }
+
+    public static LookAndFeel init() {
+        return null;
+    }
+}
